aa477ac26cdc730bb28d38183160a56bdb70fc76
Paul Lavender-Jones <paul@lavender-jones.com>
2020-07-13 02:16:40 +0100

    💄 Added 404 page

---
+++ src/routes/404.svelte
@@ -0,0 +1,10 @@
+<script>
+
+</script>
+
+<svelte:head>
+  <title>Not Found</title>
+  <meta name="description" content="Paul Lavender-Jones personal website" />
+</svelte:head>
+
+<h1>404 sorry bro</h1>
