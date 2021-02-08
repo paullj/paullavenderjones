e2554cc583c627b864e67696084a32a39e878bd8
Paul Lavender-Jones <paul@lavender-jones.com>
2021-02-08 01:35:11 +0000

    🐛 Fixed header

--- web/src/components/Header.svelte
+++ web/src/components/Header.svelte
@@ -19,24 +19,24 @@
 </script>
 
 <nav class="mb-8 text-xl font-light text sm:text-lg">
-  <ul class="flex flex-row justify-between space-x-6">
-    {#each routes as { href, name }}
-      <li
-        class="inline-block transition-colors duration-200 dark:hover:text-yellow-300 hover:text-yellow-600
+  <div class="flex flex-row justify-between">
+    <ul class="flex flex-row space-x-6">
+      {#each routes as { href, name }}
+        <li
+          class="inline-block transition-colors duration-200 dark:hover:text-yellow-300 hover:text-yellow-600
       {href ===
-        $page.path
-          ? 'text-gray-600 dark:text-blue-100'
-          : 'text-gray-400 dark:text-blue-300'}"
-      >
-        <a {href} rel="prefetch">{name}</a>
-      </li>
-    {/each}
-    <li
-      class="inline-block text-gray-400 transition-colors duration-200 dark:text-blue-300 dark:hover:text-yellow-300 hover:text-yellow-600"
+          $page.path
+            ? 'text-gray-600 dark:text-blue-100'
+            : 'text-gray-400 dark:text-blue-300'}"
+        >
+          <a {href} rel="prefetch">{name}</a>
+        </li>
+      {/each}
+    </ul>
+    <div
+      class="inline-block text-gray-400 transition-colors duration-200 sm:mr-4 dark:text-blue-300 dark:hover:text-yellow-300 hover:text-yellow-600"
     >
-      <span class="sm:mr-4">
-        <DarkModeToggle />
-      </span>
-    </li>
-  </ul>
+      <DarkModeToggle />
+    </div>
+  </div>
 </nav>
