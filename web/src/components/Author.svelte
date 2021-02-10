8d049a61f49f245688bf8f0c44dd24c9e1cdee58
Paul Lavender-Jones <paul@lavender-jones.com>
2021-02-10 00:15:03 +0000

    ♿️ Added alt to image

--- web/src/components/Author.svelte
+++ web/src/components/Author.svelte
@@ -1,10 +1,9 @@
 <script>
   export let name: string;
   export let src: string;
-  export let alt: string;
 </script>
 
 <div class="block px-2 py-1 font-medium text-gray-600 md:py-4">
-  <img {src} {alt} class="inline-block h-8 mr-2 rounded-full shadow-md" />
+  <img {src} alt="{name} thumbnail" class="inline-block h-8 mr-2 rounded-full shadow-md" />
   By {name}
 </div>
