a236bdb8d5ed027c88d06641a2f2efb860055d59
Paul Lavender-Jones <paul@lavender-jones.com>
2021-02-10 00:13:23 +0000

    💄 Improved error page

--- web/src/routes/$error.svelte
+++ web/src/routes/$error.svelte
@@ -8,10 +8,14 @@
   <title>{status}</title>
 </svelte:head>
 
-<h1>{status}</h1>
-
-<p>{error.message}</p>
+<div>
+  <h1 class="font-serif font-black text-9xl">{status}</h1>
+  <p class="mt-6 text-2xl font-medium text-gray-600">{error.message}</p>
+</div>
 
 {#if dev && error.stack}
-  <pre>{error.stack}</pre>
+  <pre class="px-4 pt-4 pb-12 mt-12 overflow-y-auto bg-gray-200 rounded-lg shadow-inner">{error.stack}</pre>
 {/if}
+<div class="mt-8">
+  <a href="/" class="text-yellow-500 border-b-2 border-yellow-500 border-dotted">Back to home</a>
+</div>
