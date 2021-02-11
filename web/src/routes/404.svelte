e4e75a78ddd8287c52a1f578123e22fc07318591
Paul Lavender-Jones <paul@lavender-jones.com>
2021-02-11 00:25:21 +0000

    💄 404 page update

--- web/src/routes/404.svelte
+++ web/src/routes/404.svelte
@@ -2,13 +2,14 @@
 </script>
 
 <svelte:head>
-  <title>404. Page not found.</title>
-  <meta name="description" content="Paul Lavender-Jones personal website" />
+  <title>404 | Page not found</title>
 </svelte:head>
-<h3 class="mt-12 mb-4 font-serif text-6xl font-bold leading-none tracking-tighter sm:mt-32">404.</h3>
+
 <div>
-  <h4 class="mb-8 text-4xl font-light">Page not found.</h4>
-  <a class="text-2xl font-medium text-red-500 focus:text-red-700 hover:underline focus:bg-red-200" href="/">
-    Back to home.
-  </a>
+  <h1 class="font-serif font-black text-9xl">404</h1>
+  <p class="mt-6 text-2xl font-medium text-gray-600">Page not found.</p>
+</div>
+
+<div class="mt-8">
+  <a href="/" class="text-yellow-500 border-b-2 border-yellow-500 border-dotted">Back to home</a>
 </div>
